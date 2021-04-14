6abcbe5ed7e1d4967169af6e4c7e82715f0be1ab
Mathew Cook <cook6348@gmail.com>
2021-04-13 19:26:29 -0500

    Playing around with arrays/list with string interpolation and averaging

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace OOP
 {
@@ -6,13 +7,24 @@ namespace OOP
     {
         static void Main(string[] args)
         {
-           CarSalesman steve = new CarSalesman("Steve", "Smith");
-           Console.WriteLine(steve.FullName);
-           steve.Sell();
+        //    CarSalesman steve = new CarSalesman("Steve", "Smith");
+        //    Console.WriteLine(steve.FullName);
+        //    steve.Sell();
 
-           RetailSalesPerson erik =  new RetailSalesPerson("Erik", "Jimbo");
-           Console.WriteLine(erik.FullName);
-           erik.Sell();
+        //    RetailSalesPerson erik =  new RetailSalesPerson("Erik", "Jimbo");
+        //    Console.WriteLine(erik.FullName);
+        //    erik.Sell();
+        var grades = new List<double>() {12.3, 45.3, 23.6, 67.3};
+        grades.Add(67.8);
+
+        var result = 0.0;
+        foreach (var num in grades)
+        {
+            result += num;
+        }
+        var average = result / grades.Count;
+        
+        Console.WriteLine("The average grade is {0}", Math.Round(average, 2));
         }
     }
 }
